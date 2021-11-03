898dfdbf5b244ecd89b4510378dc15d511a04d21
Aidi <aidi.saifudin@gmail.com>
2021-11-03 10:27:05 +0800

    parking test

--- FYPYear3Project/Assets/Script/ParkingTrigger.cs
+++ FYPYear3Project/Assets/Script/ParkingTrigger.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ParkingTrigger : MonoBehaviour
 {
     public float timerCountdown = 3.0f; //how long player have to stay in the parking lot
     public bool isPlayerColliding = false; // is the player currently at the location
+    public bool isTimer = false; //timer boolean
     public GameObject winPanel;
-    public bool isMoving = true;
+    public GameObject losePanel;
+   
 
 
 
@@ -17,35 +20,39 @@ public class ParkingTrigger : MonoBehaviour
     {
         if(isPlayerColliding == true) //collision timer
         {
+            isTimer = true;
             timerCountdown -= Time.deltaTime;
-            if(timerCountdown < 0)
+            if (timerCountdown <= 0 )
             {
                 timerCountdown = 0;
+                isTimer = false;
             }
         }
-        Debug.Log(isPlayerColliding);
+       
     }
 
     public void OnTriggerEnter(Collider other)//start the collision timer when player enter
     {
-        if(other.gameObject.tag == "Player" && isMoving == true)
-        {
-            Debug.Log("Player Entered");
-            isPlayerColliding = true;
+        //if(other.gameObject.tag == "Player" )
+        //{
+        //    Debug.Log("Player Entered");
+        //    isPlayerColliding = true;
+        //    isTimer = true;
             
-        }
+        //}
     }
 
     public void OnTriggerStay(Collider other)//check if player is still at location
     {
         if(other.gameObject.tag == "Player" && isPlayerColliding == true)
         {
+            isTimer = true;
             Debug.Log(timerCountdown);
             if(timerCountdown <= 0)
             {
-                winPanel.gameObject.SetActive(true);//panel comes out
-                isMoving = false;
-                StartCoroutine(HidePanel());//panel close
+                losePanel.gameObject.SetActive(true);//panel comes out
+                isTimer = false;
+                //StartCoroutine(HidePanel());//panel close
                 
             }
             
@@ -58,19 +65,19 @@ public class ParkingTrigger : MonoBehaviour
         {
             
             isPlayerColliding = false;
-            winPanel.gameObject.SetActive(false);//panel close
+            losePanel.gameObject.SetActive(false);//panel close
 
         }
     }
 
-    IEnumerator HidePanel()
-    {
-        yield return new WaitForSecondsRealtime(2);
-        winPanel.gameObject.SetActive(false);
-        isMoving = true;
+    //IEnumerator HidePanel()
+    //{
+    //    yield return new WaitForSecondsRealtime(2);
+    //    winPanel.gameObject.SetActive(false);
+       
         
         
         
 
-    }
+    //}
 }
