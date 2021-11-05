b736cefa36b56906d664bd354ffb30bf5240da22
WanWeiJie <weijie2288@gmail.com>
2021-11-05 11:40:54 +0800

    Passenger
    Still cannot get down from car

--- FYPYear3Project/Assets/Script/Passenger.cs
+++ FYPYear3Project/Assets/Script/Passenger.cs
@@ -8,12 +8,14 @@ public class Passenger : MonoBehaviour
     public GameObject destination;
     public Collider passengerCollider;
     public bool passengerNotInTaxi;
+    public GameObject AidiCar;
 
     // Start is called before the first frame update
     void Start()
     {
         passenger = GameObject.FindGameObjectWithTag("Passenger");
         destination = GameObject.FindGameObjectWithTag("Destination");
+        AidiCar = GameObject.FindGameObjectWithTag("Player");
         TaxiManager taxi = GetComponent<TaxiManager>();
         passengerCollider = GetComponent<Collider>();
         passengerNotInTaxi = false;
@@ -28,15 +30,15 @@ public class Passenger : MonoBehaviour
         }
     }
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("Destination") && GetComponent<TaxiManager>().passengerInTaxi == true)
-    //    {
-    //        {
-    //            passenger.transform.SetParent(p: null);
-    //            passenger.SetActive(true);
-    //            passengerNotInTaxi = true;
-    //        }
-    //    }
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && GetComponent<TaxiManager>().passengerInTaxi == true)
+        {
+            {
+                passenger.transform.SetParent(p: null);
+                passenger.SetActive(true);
+                passengerNotInTaxi = true;
+            }
+        }
+    }
 }
