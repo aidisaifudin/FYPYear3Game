e8660a9ca41468a98b44bdfdfb417f9231452fa2
Toastyship <64509653+Toastyship@users.noreply.github.com>
2021-11-11 09:49:24 +0800

    Scenario

--- FYPYear3Project/Assets/Script/TriggerDialogue.cs
+++ FYPYear3Project/Assets/Script/TriggerDialogue.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TriggerDialogue : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float m_Thrust = 300f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Fetch the Rigidbody from the GameObject with this script attached
+       // rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            rb.AddForce(transform.forward * m_Thrust);
+            Debug.Log("Forward");
+        }
     }
 }
